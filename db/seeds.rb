f7531183aaadf48355a57cf708175097d1f5d9d8
Dushyant kumar <dushyant.kumar@1mg.com>
2018-09-25 13:06:07 +0530

    created seeds for user and question model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Users
+User.create!(first_name:  "example",
+             last_name: "user",
+             email: "example@railstutorial.org",
+             phone_number: "9898898",
+             password_digest: "foobar")
+
+49.times do |n|
+  first_name  = "sdfsdf"
+  last_name =  "sdfsd"
+  email = "example-#{n+1}@railstutorial.org"
+  phone_number = "2224333"
+  password_digest = "password"
+  User.create!(first_name:  first_name,
+               last_name: last_name,
+               email: email,
+               phone_number: phone_number,
+               password_digest:password_digest)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  base_question = "What is java question"
+  status = "new"
+  users.each do |user| question = base_question +" "+ user.id.to_s
+  user.questions.create!(question: question, status: status)
+  end
+end

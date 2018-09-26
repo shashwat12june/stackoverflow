9423c60677bdedbb63834d001934963b9545ed87
Dushyant kumar <dushyant.kumar@1mg.com>
2018-09-26 17:27:39 +0530

    user profile added partials for userquestion added

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -19,6 +19,10 @@ class UserController < ApplicationController
     end
   end
 
+  def show
+    @user=User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
   validates :password, presence: true, confirmation: true
   validates_confirmation_of :password
   has_secure_password
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 end
